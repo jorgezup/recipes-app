a846d7779d28ff8ff751b82ac5af08f12896bf12
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-29 18:58:58 -0400

    adicionado requisicoes de foods e drinks e criado um card generico

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import SearchHeader from '../components/SearchHeader';
 import Layout from '../components/Layout';
+import Card from '../components/Card';
+
+const LIMIT_DRINKS = 12;
 
 const Drinks = () => {
   const { location } = useHistory();
+  const [allDrinks, setAllDrinks] = useState([]);
+
+  const fetchDrinks = useCallback(async () => {
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    const filteredMeals = data.drinks.slice(0, LIMIT_DRINKS);
+    setAllDrinks(filteredMeals);
+  }, []);
+
+  useEffect(() => {
+    fetchDrinks();
+  }, [fetchDrinks]);
+
   return (
     <Layout title="Drinks">
       <SearchHeader location={ location } />
+      {
+        allDrinks.map((drink, index) => (
+          <Card
+            index={ index }
+            key={ drink.idDrink }
+            image={ drink.strDrinkThumb }
+            title={ drink.strDrink }
+          />
+        ))
+      }
     </Layout>
   );
 };
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import SearchHeader from '../components/SearchHeader';
 import Layout from '../components/Layout';
 import RecipeSearch from '../components/RecipeSearch';
+import Card from '../components/Card';
+
+const LIMIT_RECIPES = 12;
 
 const Foods = () => {
   const history = useHistory();
+  const [allRecipes, setAllRecipes] = useState([]);
+
   const { location } = history;
+
   const searchClicked = useSelector((state) => state.searchClicked);
   const { meals } = useSelector((state) => state.foodSearch);
   const filteredClicked = useSelector((state) => state.filteredClicked);
+
+  const fetchRecipes = useCallback(async () => {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    const filteredMeals = data.meals.slice(0, LIMIT_RECIPES);
+    setAllRecipes(filteredMeals);
+  }, []);
+
+  useEffect(() => {
+    fetchRecipes();
+  }, [fetchRecipes]);
+
   return (
     <Layout title="Foods">
       {searchClicked && <SearchHeader location={ location } />}
@@ -22,6 +40,16 @@ const Foods = () => {
             history={ history }
           />
         )
+      }
+      {
+        allRecipes.map((recipe, index) => (
+          <Card
+            index={ index }
+            key={ recipe.idMeal }
+            image={ recipe.strMealThumb }
+            title={ recipe.strMeal }
+          />
+        ))
       }
 
     </Layout>
