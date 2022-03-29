19ef5e889e11daf7ff615ebbbc35ebe9bc0157f9
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-29 00:32:00 -0400

    alterado rotas dentro do arquivo app

--- src/App.js
+++ src/App.js
@@ -2,30 +2,38 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Foods from './pages/Foods';
+import Food from './pages/Food';
 import Drinks from './pages/Drinks';
+import Drink from './pages/Drink';
 import Explore from './pages/Explore';
 import Profile from './pages/Profile';
-import DoneReciples from './pages/DoneReciples';
+import DoneRecipes from './pages/DoneRecipes';
 import Favorites from './pages/Favorites';
+import ExploreFoods from './pages/ExploreFoods';
+import ExploreDrinks from './pages/ExploreDrinks';
+import FoodInProgress from './pages/FoodInProgress';
+import DrinkInProgress from './pages/DrinkInProgress';
+import Ingredients from './pages/Ingredients';
+import Nationalities from './pages/Nationalities';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/drinks" component={ Drinks } />
-      <Route exact path="/foods/:id" component={ Foods } />
-      <Route exact path="/drinks/:id" component={ Foods } />
-      <Route exact path="/foods/:id/in-progress" component={ Foods } />
-      <Route exact path="/drinks/:id/in-progress" component={ Foods } />
-      <Route path="/foods" component={ Foods } />
+      <Route exact path="/drinks" component={ Drinks } />
+      <Route exact path="/foods/:id" component={ Food } />
+      <Route exact path="/drinks/:id" component={ Drink } />
+      <Route path="/foods/:id/in-progress" component={ FoodInProgress } />
+      <Route path="/drinks/:id/in-progress" component={ DrinkInProgress } />
+      <Route exact path="/foods" component={ Foods } />
       <Route exact path="/explore" component={ Explore } />
-      <Route path="/explore/foods" component={ Explore } />
-      <Route path="/explore/drinks" component={ Explore } />
-      <Route path="/explore/foods/ingredients" component={ Explore } />
-      <Route path="/explore/drinks/ingredients" component={ Explore } />
-      <Route path="/explore/foods/nationalities" component={ Explore } />
+      <Route exact path="/explore/foods" component={ ExploreFoods } />
+      <Route exact path="/explore/drinks" component={ ExploreDrinks } />
+      <Route path="/explore/foods/ingredients" component={ Ingredients } />
+      <Route path="/explore/foods/nationalities" component={ Nationalities } />
+      <Route path="/explore/drinks/ingredients" component={ Ingredients } />
       <Route path="/profile" component={ Profile } />
-      <Route path="/done-recipes" component={ DoneReciples } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
       <Route path="/favorite-recipes" component={ Favorites } />
     </Switch>
   );
