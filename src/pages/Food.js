22805bd52e1dc764e5841622d30bfd6fbc2cfd4a
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-30 17:30:04 -0400

    criado lógica para mostrar detalhes de uma única comida

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -1,7 +1,81 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useEffect, useCallback, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Details from '../components/Details';
+import Loading from '../components/Loading';
 
-const Food = () => (
-  <div>Food</div>
-);
+const LIMIT_DRINKS = 6;
+
+const Food = () => {
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState();
+  const [recommendations, setRecommendations] = useState();
+
+  const fetchRecipe = useCallback(async () => {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
+    );
+    const { meals } = await response.json();
+    const objRecipe = meals[0];
+    const ingredients = Object.keys(objRecipe)
+      .filter((key) => key.includes('strIngredient'))
+      .reduce((cur, key) => {
+        if (objRecipe[key] !== '')cur.push(objRecipe[key]);
+        return cur;
+      }, []);
+    const measure = Object.keys(objRecipe)
+      .filter((key) => key.includes('strMeasure'))
+      .reduce((cur, key) => {
+        if (objRecipe[key] !== '')cur.push(objRecipe[key]);
+        return cur;
+      }, []);
+    const ingredientsAndMeasure = ingredients.reduce((acc, cur, i) => {
+      acc.push({
+        ingrediente: cur,
+        measure: measure[i],
+      });
+      return acc;
+    }, []);
+    const recipeObj = {
+      ...objRecipe,
+      ingredientsAndMeasure,
+      videoId: objRecipe?.strYoutube.split('=')[1],
+      name: objRecipe.strMeal,
+      image: objRecipe.strMealThumb,
+    };
+    setRecipe(recipeObj);
+  }, [id]);
+
+  const fetchRecommendations = useCallback(async () => {
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    const { drinks } = await response.json();
+    const filteredDrinks = drinks.slice(0, LIMIT_DRINKS);
+
+    const recommendationFiltered = filteredDrinks.reduce((acc, cur) => {
+      acc.push({
+        name: cur.strDrink,
+        image: cur.strDrinkThumb,
+      });
+      return acc;
+    }, []);
+    setRecommendations(recommendationFiltered);
+  }, []);
+
+  useEffect(() => {
+    fetchRecipe();
+    fetchRecommendations();
+  }, [fetchRecipe, fetchRecommendations]);
+
+  return (
+    <div>
+      {recipe && recommendations
+        ? <Details recipe={ recipe } recommendations={ recommendations } />
+        : <Loading />}
+    </div>);
+};
+
+Food.propTypes = {
+  recipe: PropTypes.arrayOf(Object),
+}.isRequired;
 
 export default Food;
