a6459b77a4d87e5294bf49164688d8d2502b7fd4
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-31 18:12:21 -0400

    criado componente card in progress

--- src/pages/FoodInProgress.js
+++ src/pages/FoodInProgress.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+// import CardInProgress from '../components/CardInProgress';
+import { getMealByIdThunk } from '../Redux/actions/mealById';
+import Loading from '../components/Loading';
+import { setIngredientsArray } from '../utils';
 
-const FoodInProgress = () => (
-  <div>Food In Progress</div>
-);
+const FoodInProgress = () => {
+  const { id } = useParams();
+  console.log(id);
+  const dispatch = useDispatch();
+  const { meals, isFetching } = useSelector((state) => state.mealById);
+  console.log(meals);
+
+  console.log(setIngredientsArray(meals[0]));
+
+  const recipe = {
+    title: meals[0].strMeal,
+    image: meals[0].strMealThumb,
+    category: meals[0].strCategory,
+    instructions: meals[0].strInstructions,
+    // ingredients: setIngredientsArray(meals[0]),
+  };
+
+  console.log(recipe);
+
+  useEffect(() => {
+    dispatch(getMealByIdThunk(id));
+  }, [dispatch, id]);
+
+  return (
+    <div>
+      {
+        isFetching && <Loading />
+        // : <CardInProgress />
+      }
+    </div>
+  );
+};
 
 export default FoodInProgress;
