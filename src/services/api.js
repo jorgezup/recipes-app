774a6d1c01655c4425d897b8bf45258636ffd81f
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-31 00:59:28 -0400

    ajustado rota da api

--- src/services/api.js
+++ src/services/api.js
@@ -29,7 +29,7 @@ export const fetchMealById = async (mealId) => {
 };
 
 export const fetchDrinkById = async (drinkId) => {
-  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${drinkId}`);
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`);
   const { drinks } = await response.json();
   return drinks;
 };
