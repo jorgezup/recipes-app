14eff22db0bb833bff6b06fa6014fd7863f10633
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-04-04 16:20:47 -0400

    inserido logica de compartilhar e de setar como favorita uma receita

--- src/components/CardInProgress.jsx
+++ src/components/CardInProgress.jsx
@@ -1,14 +1,32 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import clipboardCopy from 'clipboard-copy';
+import { useHistory } from 'react-router-dom';
 import {
   setMealInProgress,
   removeMealInProgress,
   getRecipesInProgress } from '../services/localStorage/recipesInProgress';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeart from '../images/whiteHeartIcon.svg';
+import blackHeart from '../images/blackHeartIcon.svg';
+import {
+  getFromLocalStorage,
+  removeRecipeFromFavorite,
+  setRecipesInFavorite,
+} from '../services/localStorage/favoriteRecipes';
 
 const CardInProgress = ({ recipe }) => {
-  // const [checkIngredient, setCheckIngredient] = useState(true);
+  const history = useHistory();
   const [ingredientNames, setIngredientNames] = useState([]);
-  // const toggleCheckBox = () => setCheckIngredient(!checkIngredient);
+  const [isRecipeShared, setIsRecipeShared] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    const recipesAlreadyFavorited = getFromLocalStorage('favoriteRecipes');
+    const isThisRecipeFavorite = recipesAlreadyFavorited
+      .some(({ id }) => id === recipe.id);
+    setIsFavorite(isThisRecipeFavorite);
+  }, [recipe.id]);
 
   const ingredientFilter = (id, ingredient) => {
     if (!ingredientNames.includes(ingredient)) {
@@ -21,6 +39,40 @@ const CardInProgress = ({ recipe }) => {
   const handleCheckIngredient = (id, ingredient) => {
     setIngredientNames([...ingredientNames, ingredient]);
     ingredientFilter(id, ingredient);
+  };
+
+  const path = history.location.pathname.split('/')[1];
+
+  const handleShare = () => {
+    clipboardCopy(`http://localhost:3000/${path}/${recipe.id}`);
+    setIsRecipeShared(true);
+  };
+
+  const setRecipeFavorite = (recipeObject) => {
+    setRecipesInFavorite(recipeObject);
+  };
+
+  const removeRecipeAsFavorite = (id) => {
+    removeRecipeFromFavorite(id);
+  };
+
+  const handleFavorite = () => {
+    if (isFavorite === false) {
+      const recipeObject = {
+        id: recipe.id,
+        type: path === 'foods' ? 'food' : 'drink',
+        nationality: recipe.nationality || '',
+        category: recipe.category,
+        alcoholicOrNot: recipe.alcoholic || '',
+        name: recipe.title,
+        image: recipe.image,
+      };
+      setRecipeFavorite(recipeObject);
+      setIsFavorite(true);
+    } else {
+      removeRecipeAsFavorite(recipe.id);
+      setIsFavorite(false);
+    }
   };
 
   const { meals: localStorage } = getRecipesInProgress();
@@ -52,11 +104,18 @@ const CardInProgress = ({ recipe }) => {
         ))}
       </ul>
       <p data-testid="instructions">{recipe.instructions}</p>
-      <button data-testid="share-btn" type="button">
-        Compartilhar
+      <button type="button" onClick={ handleShare }>
+        <img src={ shareIcon } data-testid="share-btn" alt="icon-share" />
       </button>
-      <button data-testid="favorite-btn" type="button">
-        Favoritar
+      {
+        isRecipeShared && <span>Link copied!</span>
+      }
+      <button type="button" onClick={ handleFavorite }>
+        <img
+          data-testid="favorite-btn"
+          src={ isFavorite ? blackHeart : whiteHeart }
+          alt="icon-favorite"
+        />
       </button>
       <button data-testid="finish-recipe-btn" type="button">
         Finalizar
