594185621bfd30aa8ac3526ace3b2d94f68dd654
joao <joao.pedro.ramos@outlook.com>
2022-04-06 18:03:07 +0100

    Resolve botao de compartilhar url

--- src/components/DoneFoods.jsx
+++ src/components/DoneFoods.jsx
@@ -1,44 +1,52 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import clipboardCopy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
 
-const DoneFoods = ({ recipe, index }) => (
-  <section key={ recipe.id }>
-    <img
-      src={ recipe.image }
-      alt={ recipe.name }
-      data-testid={ `${index}-horizontal-image` }
-    />
-    <h2 data-testid={ `${index}-horizontal-name` }>{ recipe.name }</h2>
-    <h3 data-testid={ `${index}-horizontal-top-text` }>
-      { recipe.nationality }
-      { ' - ' }
-      { recipe.category }
-    </h3>
-    <p data-testid={ `${index}-horizontal-done-date` }>{ recipe.doneDate }</p>
-    <button
-      type="button"
-      onClick={ () => clipboardCopy(
-        `http://localhost:3000/foods/${recipe.id}`,
-      ) }
-    >
+const DoneFoods = ({ recipe, index }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const clickCopyButton = () => {
+    clipboardCopy(`http://localhost:3000/foods/${recipe.id}`);
+    setIsCopied(true);
+  };
+
+  return (
+    <section key={ recipe.id }>
       <img
-        data-testid={ `${index}-horizontal-share-btn` }
-        src={ shareIcon }
-        alt="icon-share"
+        src={ recipe.image }
+        alt={ recipe.name }
+        data-testid={ `${index}-horizontal-image` }
       />
-    </button>
-    {recipe.tags.map((tag) => (
-      <p
-        key={ tag }
-        data-testid={ `${index}-${tag}-horizontal-tag` }
+      <h2 data-testid={ `${index}-horizontal-name` }>{ recipe.name }</h2>
+      <h3 data-testid={ `${index}-horizontal-top-text` }>
+        { recipe.nationality }
+        { ' - ' }
+        { recipe.category }
+      </h3>
+      <p data-testid={ `${index}-horizontal-done-date` }>{ recipe.doneDate }</p>
+      <button
+        type="button"
+        onClick={ () => clickCopyButton() }
       >
-        { tag }
-      </p>
-    ))}
-  </section>
-);
+        <img
+          data-testid={ `${index}-horizontal-share-btn` }
+          src={ shareIcon }
+          alt="icon-share"
+        />
+      </button>
+      { isCopied && <p>Link copied!</p> }
+      {recipe.tags.map((tag) => (
+        <p
+          key={ tag }
+          data-testid={ `${index}-${tag}-horizontal-tag` }
+        >
+          { tag }
+        </p>
+      ))}
+    </section>
+  );
+};
 
 DoneFoods.propTypes = {
   recipe: PropTypes.shape({
