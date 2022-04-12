2cd30fafcfc51bade6d83ae1716edfbd6162ffab
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-04-12 17:03:45 -0400

    fix lint problems

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useHistory, Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
